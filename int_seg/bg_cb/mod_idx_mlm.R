ea3937312f15dbb6c8166cd4028a939badf33fb2
Kimberly Nestor <kimberlynestor@alva.psy.cmu.edu>
2022-08-09 20:24:08 -0400

    update int_seg code, add R

--- int_seg/bg_cb/mod_idx_mlm.R
+++ int_seg/bg_cb/mod_idx_mlm.R
@@ -1,18 +1,35 @@
-# 
-#
-#
-#
+# Kimberly Nestor
+# 08.2022
+# This program takes as input a csv file with subject, task and mean modularity 
+# index block information and inputs that data into a mixed linear model.
 
 
-
-# .libPaths('/home/kimberlynestor/Rlib')
-# .libPaths('/tmp/Rtmp1A8u3u/downloaded_packages')
-
-install.packages('lmerTest')
-#install.packages('nloptr')
-
+# install.packages(‘lmerTest’)
+# install.packages('broom') # glance()
 library(lmerTest)
+library(lme4)
+library(broom)
 
-# read.csv('on_task_block_mu_mod_idx.csv')
+filepath = '/home/kimberlynestor/gitrepo/int_seg/bg_cb'
 
-lmer()
+# on task load data
+on.task.mod.idx = read.csv(file=file.path(filepath, 'on_task_block_mu_mod_idx.csv'), header=TRUE)
+# head(on.task.mod.idx)
+
+# run on task mixed linear model
+on.task.mlm.sum = summary(lmer(mu_mod_idx ~ task + block + task:block + 
+                                 (task | subj_ID), on.task.mod.idx))
+
+write.csv(on.task.mlm.sum$coefficients, file.path(filepath, 'on_task_mod_idx_mlm_output.csv'))
+
+
+# off task load data
+off.task.mod.idx = read.csv(file=file.path(filepath, 'off_task_block_mu_mod_idx.csv'), header=TRUE)
+# head(off.task.mod.idx)
+
+# run off task mixed linear model
+off.task.mlm.sum = summary(lmer(mu_mod_idx ~ task + block + task:block + 
+                                 (task | subj_ID), off.task.mod.idx))
+off.task.mlm.sum
+
+
