f5222550b91fa515e577c9dd03cde431015502d9
Kimberly Nestor <kimberlynestor@alva.psy.cmu.edu>
2022-08-09 20:20:21 -0400

    update int_seg code, add R

--- int_seg/bg_cb/mod_idx_all_mlr.py
+++ int_seg/bg_cb/mod_idx_all_mlr.py
@@ -36,6 +36,8 @@ from statsmodels.tsa.ar_model import AutoReg
 from scipy.stats import zscore
 from scipy import stats
 import scipy.stats as st
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 from nilearn.image import load_img
 from nilearn.glm.first_level import compute_regressor
@@ -44,12 +46,14 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
 import ptitprince as pt
+from matplotlib.lines import Line2D
 
-np.set_printoptions(threshold=sys.maxsize)
+# np.set_printoptions(threshold=sys.maxsize)
 # pd.set_option('display.max_rows', None)
 
 
-p_dict = {'Incongruent':'tab:orange', 'Congruent':'tab:blue', 'Difference':'#8A2D1C'}
+p_dict = {'Incongruent':'tab:orange', 'Congruent':'tab:blue', 'Fixation':'tab:green', \
+          'Difference':'#8A2D1C', 'Incongruent Fixation':'tab:orange', 'Congruent Fixation':'tab:blue'}
 
 
 # data path and subjects
@@ -128,7 +132,6 @@ q_avg_smooth_z = stats.zscore(q_avg_smooth)
 # init_mask = np.ma.array(np.ones(5), mask=np.ones(5))
 
 
-
 ## TASK BLOCK ONSET
 df_task_events = pd.read_csv(opj(main_dir, dep_path, 'task-stroop_events.tsv'), sep="\t")
 
@@ -150,19 +153,27 @@ fix_cond = np.array( [df_task_events['onset'].to_numpy() - rest] + \
 # task timing blocks - modularity
 inc_block = list(zip(inc_cond[0], inc_cond[0]+inc_cond[1][0]))
 con_block = list(zip(con_cond[0], con_cond[0]+con_cond[1][0]))
+fix_block = list(zip(fix_cond[0], fix_cond[0]+fix_cond[1][0]))
 
-# indices for inc and con
+# indices for inc and con, fix
 inc_block_frames = np.array(inc_block)/2
 inc_frames_idx = np.array(list(map(lambda i: list(range(int(i[0]), int(i[1]))), inc_block_frames))) -5
 
 con_block_frames = np.array(con_block)/2
 con_frames_idx = np.array(list(map(lambda i: list(range(int(i[0]), int(i[1]))), con_block_frames))) -5
 
+fix_block_frames = np.array(fix_block)/2
+fix_frames_idx = np.array(list(map(lambda i: list(range(int(i[0]), int(i[1]))), fix_block_frames)))[1:] -5
+
+
 # for all subjects get only inc and con mod
 q_allsub_inc = np.array(list(map(lambda subj: np.array(list(map(lambda block: \
-                                    subj[block] ,inc_frames_idx))) ,q_allsub_z))) # q_allsub, q_allsub_z, q_allsub_smooth_z
+                                    subj[block] , inc_frames_idx))) ,q_allsub_z))) # q_allsub, q_allsub_z, q_allsub_smooth_z
 q_allsub_con = np.array(list(map(lambda subj: np.array(list(map(lambda block: \
-                                    subj[block] ,con_frames_idx))) ,q_allsub_z)))
+                                    subj[block] , con_frames_idx))) ,q_allsub_z)))
+q_allsub_fix = np.array(list(map(lambda subj: np.array(list(map(lambda block: \
+                                    subj[block] , fix_frames_idx))) ,q_allsub_z)))
+
 
 # smooth inc and con block data, make dataframe for plotting - to do z score
 q_inc_avg = np.mean(q_allsub_inc, axis=0)
@@ -180,18 +191,134 @@ q_con_sep_blocks = list(map(lambda xxx:[xxx[0], int(xxx[1]), 'Congruent'], \
 q_con_sep_blocks = list(map(lambda x: [x[0], x[1]+x[1], x[2]], q_con_sep_blocks))
 
 
-df_q_sep_blocks = pd.DataFrame(list(itertools.chain(q_inc_sep_blocks, q_con_sep_blocks)), \
+q_fix_avg = np.mean(q_allsub_fix, axis=0)
+q_fix_sep_blocks = list(map(lambda xxx:[xxx[0], int(xxx[1]), 'Fixation'], \
+                        np.array([np.array(list(map(lambda x: [x, i[0]], i[1])))  \
+                            for i in np.array(list(enumerate(q_fix_avg, 9))  , \
+                                dtype=object )]).reshape((35, 2))))
+q_fix_off_task_sep = list(map(lambda i: [i[0], i[1], 'Incongruent Fixation'] \
+                            if i[1] in range(9, 16, 2) else [i[0], i[1], 'Congruent Fixation'], \
+                              q_fix_sep_blocks))
+
+df_q_sep_blocks = pd.DataFrame(list(itertools.chain(q_inc_sep_blocks, q_con_sep_blocks, q_fix_sep_blocks)), \
                                columns=['mod_idx', 'block', 'task'])
 
+df_task = df_q_sep_blocks[(df_q_sep_blocks['task'] == 'Incongruent') | \
+                          (df_q_sep_blocks['task'] == 'Congruent')]
+df_fix = pd.DataFrame(q_fix_off_task_sep, columns=['mod_idx', 'block', 'task'])
+# print( [f'F{i}' for i in range(1, 8)] )
 
-###### POINTPLOT OF CI
-sns.pointplot(  x='block', y='mod_idx', hue='task', data=df_q_sep_blocks , \
-                join=False, palette=p_dict )
-plt.savefig('subjs_all_net_cort/mod/allsub_cortnet_mod_qavg_smooth_sig1_pointplot_ci.png', dpi=300)
+###### POINTPLOT OF CI - all in one
+inc_patch = Line2D(range(1), range(1), color='white', marker='o', markersize=11, \
+                   markerfacecolor='tab:orange')
+con_patch = Line2D(range(1), range(1), color='white', marker='o', markersize=11, \
+                   markerfacecolor='tab:blue')
+fix_patch = Line2D(range(1), range(1), color='white', marker='o', markersize=11, \
+                   markerfacecolor='tab:green')
+
+# fig, ax = plt.subplots()
+sns.pointplot(x='block', y='mod_idx', hue='task', data=df_q_sep_blocks, \
+              join=False, palette=p_dict, order=list(sum(zip(range(1,9), \
+                                                range(9,16)), ()))+[8] )
+plt.xticks(np.arange(0,15), labels=list(sum(zip(range(1,9), ['']*7), ()))+[8])
+# plt.xticks(np.arange(0,14), labels=range(1,15))
+# ax.set_xticklabels(range(1,15))
+plt.xlabel("Task block", size=14, fontname="serif")
+plt.ylabel("Modularity (Q)", size=14, fontname="serif")
+plt.legend((inc_patch, con_patch, fix_patch), ('Incongruent', 'Congruent', 'Fixation'), numpoints=1)
+plt.tight_layout()
+plt.savefig('subjs_all_net_cort/mod/allsub_cortnet_mod_qavg_smooth_sig1_pointplot_ci_wr.png', dpi=300)
 plt.show()
 
 
-##############
+###### POINTPLOT OF CI - two subplots
+
+
+# fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={'width_ratios':[2,3]})
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,5))
+sns.pointplot(ax=ax1, x='block', y='mod_idx', hue='task', data=df_task, \
+                            join=False, palette=p_dict)
+sns.pointplot(ax=ax2, x='block', y='mod_idx', hue='task', data=df_fix, \
+                            join=False, palette=p_dict)
+plt.xticks(np.arange(0,7), labels=[f'F{i}' for i in range(1, 8)])
+ax1.set_xlabel("On Task Block", size=12, fontname="serif")
+ax2.set_xlabel("Off Task Block", size=12, fontname="serif")
+ax1.set_ylabel("Modularity (Q)", size=12, fontname="serif")
+ax2.set_ylabel('')
+ax1.legend((inc_patch, con_patch), ('Incongruent', 'Congruent'), numpoints=1, loc=1)
+ax2.legend('', frameon=False)
+fig.legend('', frameon=False)
+plt.tight_layout()
+plt.savefig('subjs_all_net_cort/mod/allsub_cortnet_mod_qavg_smooth_sig1_pointplot_ci_subplots.png', dpi=300)
+plt.show()
+
+
+###### MIXED EFFECTS - con=1, incon=0
+# ON TASK
+# prepare melt df with subj_id, task, block and mean mod_idx by block
+q_allsub_con_tmean = np.array(list(map(lambda sub: np.mean(sub, axis=1), q_allsub_con)))
+df_mlm_con = pd.DataFrame(q_allsub_con_tmean, columns=range(1,5))
+df_mlm_con.insert(0, 'subj_ID', [int(i) for i in subj_lst])
+df_mlm_con['task'] = 'Congruent'#1
+df_mlm_con_melt = pd.melt(df_mlm_con, id_vars=['subj_ID', 'task'], var_name='block', \
+                          value_name='mu_mod_idx')
+
+q_allsub_inc_tmean = np.array(list(map(lambda sub: np.mean(sub, axis=1), q_allsub_inc)))
+df_mlm_inc = pd.DataFrame(q_allsub_inc_tmean, columns=range(1,5))
+df_mlm_inc.insert(0, 'subj_ID', [int(i) for i in subj_lst])
+df_mlm_inc['task'] = 'Incongruent'#0
+df_mlm_inc_melt = pd.melt(df_mlm_inc, id_vars=['subj_ID', 'task'], var_name='block', \
+                          value_name='mu_mod_idx')
+
+# concat con and inc melt df
+df_mlm = pd.concat([df_mlm_con_melt, df_mlm_inc_melt], ignore_index=True)
+# print(df_mlm)
+df_mlm.to_csv('on_task_block_mu_mod_idx.csv', index=False)
+
+# run mixed effects model - on task, inc and con
+# vcf = {'task': '0 + C(task)', 'block': '0 + C(block)'} #, vc_formula=vcf)
+# md = smf.mixedlm('mu_mod_idx ~ task + block + task:block', df_mlm, \
+#                  groups=df_mlm['subj_ID'], re_formula='~task') # task:block
+# md = smf.mixedlm('mu_mod_idx ~ C(task) + block + C(task):block', df_mlm, \
+#                  groups=df_mlm['subj_ID'], re_formula='~C(task)')
+# mdf = md.fit() # method=['powell', 'lbfgs', 'bfgs']
+# print(mdf.summary())
+
+
+# OFF TASK
+"""
+q_fix_off_task_sep_blocks_allsub = np.array(list(map(lambda sub: np.array([\
+    np.array(list(map(lambda x: [x, i[0]], i[1]))) for i in np.array(\
+        list(enumerate(sub, 1)), dtype=object)]).reshape((35, 2)), q_allsub_fix)))
+q_fix_off_task_sep_allsub = list(map(lambda sub: list(map(lambda i: [i[0], int(i[1]), \
+        'Incongruent Fixation'] if i[1] in range(1, 8, 2) else [i[0], int(i[1]), \
+                'Congruent Fixation'], sub)), q_fix_off_task_sep_blocks_allsub))
+"""
+q_allsub_fix_tmean = np.array(list(map(lambda sub: np.mean(sub, axis=1), q_allsub_fix)))
+
+q_allsub_fix_inc_tmean = list(map(lambda sub: sub[list(range(0,6,2))], q_allsub_fix_tmean))
+df_mlm_inc_off_task = pd.DataFrame(q_allsub_fix_inc_tmean, columns=range(1,4))
+df_mlm_inc_off_task.insert(0, 'subj_ID', [int(i) for i in subj_lst])
+df_mlm_inc_off_task['task'] = 'Incongruent'
+df_mlm_inc_melt_off = pd.melt(df_mlm_inc_off_task, id_vars=['subj_ID', 'task'], var_name='block', \
+                          value_name='mu_mod_idx')
+
+q_allsub_fix_con_tmean = list(map(lambda sub: sub[list(range(1,7,2))], q_allsub_fix_tmean))
+df_mlm_con_off_task = pd.DataFrame(q_allsub_fix_con_tmean, columns=range(1,4))
+df_mlm_con_off_task.insert(0, 'subj_ID', [int(i) for i in subj_lst])
+df_mlm_con_off_task['task'] = 'Congruent'
+df_mlm_con_melt_off = pd.melt(df_mlm_con_off_task, id_vars=['subj_ID', 'task'], var_name='block', \
+                          value_name='mu_mod_idx')
+
+# concat con and inc melt df
+df_mlm_off_task = pd.concat([df_mlm_con_melt_off, df_mlm_inc_melt_off], ignore_index=True)
+# print(df_mlm_off_task)
+df_mlm_off_task.to_csv('off_task_block_mu_mod_idx.csv', index=False)
+
+sys.exit()
+
+
+###### MODULARITY LINE GRAPH
 # legend patches
 inc_patch = mpatches.Patch(color='tab:orange', label='Incongruent', alpha=0.35)
 con_patch = mpatches.Patch(color='tab:blue', label='Congruent', alpha=0.35)
@@ -375,5 +502,5 @@ plt.savefig('subjs_all_net_cort/mlr/allsub_cortnet_stripplot_diff.png', dpi=300)
 plt.show()
 
 
-print(df_coeffs)
+# print(df_coeffs)
 # C - I
---
+++ int_seg/bg_cb/mod_idx_mlm.R
@@ -0,0 +1,18 @@
+# 
+#
+#
+#
+
+
+
+# .libPaths('/home/kimberlynestor/Rlib')
+# .libPaths('/tmp/Rtmp1A8u3u/downloaded_packages')
+
+install.packages('lmerTest')
+#install.packages('nloptr')
+
+library(lmerTest)
+
+# read.csv('on_task_block_mu_mod_idx.csv')
+
+lmer()
