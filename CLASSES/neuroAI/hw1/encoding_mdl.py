c8e3622b68d647b4eb312b0e19f3b55ae69e641b
Kimberly Nestor <kimberly.a.nestor@gmail.com>
2024-03-07 16:04:01 -0500

    update encoding.py

--- CLASSES/neuroAI/hw1/encoding_mdl.py
+++ CLASSES/neuroAI/hw1/encoding_mdl.py
@@ -45,10 +45,9 @@ story_names = sorted(os.listdir(story_save_path))
 
 # mask = load_img(curr_dir + dir_depth + mask_dir + 'mask_cortical.nii.gz').get_fdata()
 
-
 #### 1.3 Make encoding model
 # load embeddings
-all_mdl_embeds = [np.load(f'{new_embed_save_path}{i}') for i in os.listdir(new_embed_save_path)]
+all_mdl_embeds = [np.load(f'{new_embed_save_path}{i}') for i in sorted(os.listdir(new_embed_save_path))]
 train_mdl_embeds = all_mdl_embeds[:-1]
 test_mdl_embeds = all_mdl_embeds[-1]
 
@@ -64,26 +63,28 @@ test_fmri_mats = all_fmri_mats[-1]
 CV_ridge = SimpleCVRidgeDiffLambda(solver = 'sag', n_splits=10) # init solver = 'auto'
 
 # train and save mdl weights and lambdas
-"""
 story_param_lst = []
 for story in range(len((train_mdl_embeds))):
-    # print(f'story = {story}')
+    print(f'story = {story}')
+    
     subj_param_lst = []
     for subj in range(train_mdl_embeds[story].shape[0]):
-        # print(subj)
+        print(subj)
         ridge_train = CV_ridge.fit(train_mdl_embeds[story][subj], train_fmri_mats[story][subj]) # X, Y
         subj_param_lst.append(ridge_train)
-        # np.save(f'{mdl_output_dir}{story_names[story]}_coeffs_{subj+1}.npy', ridge_train[0])
-        # np.save(f'{mdl_output_dir}{story_names[story]}_lambda_{subj+1}.npy', ridge_train[1])
+        np.save(f'{mdl_output_dir}{story_names[story]}_coeffs_{subj+1}.npy', ridge_train[0])
+        np.save(f'{mdl_output_dir}{story_names[story]}_lambda_{subj+1}.npy', ridge_train[1])
     story_param_lst.append(subj_param_lst)
-"""
+
 
 # predict fmri data from test mdl embed, save for each subj
-"""
+print(test_mdl_embeds.shape)
 for i in range(len(test_mdl_embeds)):
     predict_fmri = CV_ridge.predict(test_mdl_embeds[i])
-    # np.save(f'{pred_output_dir}{story_names[-1]}_pred{i+1}.npy', predict_fmri)
-"""
+    print('test shape', test_mdl_embeds[i].shape)
+    print('pred shape', predict_fmri.shape)
+    np.save(f'{pred_output_dir}{story_names[-1]}_pred{i+1}.npy', predict_fmri)
+
 
 # training data names
 train_data_names = sorted(os.listdir(mdl_output_dir))
@@ -93,8 +94,8 @@ train_data_names_lam = [i for i in train_data_names if 'lambda' in i]
 # fmri predictions names
 fmri_pred_names = sorted(os.listdir(pred_output_dir))
 
-"""
-#### make brain plots in pycortex
+
+#### make brain plots in nilearn
 ## plot 1 = correlation of encoding prediction and regularization coefficients - for each subj test data (3), for each story coeffs(8*3)
 for i in range(len(fmri_pred_names)):
     # load fmri test predictions
@@ -113,7 +114,7 @@ for i in range(len(fmri_pred_names)):
         plt.savefig(f'{output_figs_dir}{m_save_name}_pred{i+1}.png', dpi=500)
         # plt.show()
         plt.close()
-"""
+
 
 ## plot 2 = 1 subj, 1 ft space (story), brain plot of optimal regularization parameter (lambda)
 mdl_lambda = np.load(f'{mdl_output_dir}{train_data_names_lam[0]}')
@@ -126,7 +127,7 @@ l_save_name = train_data_names_lam[0].split('.')[0]
 plotting.plot_surf(hcp.mesh.flat, hcp.cortex_data(np.array(list(mdl_lambda)*mdl_lambda.shape[0])), colorbar=True, cmap='viridis') # , vmin=-0.27, vmax=0.7
 plt.xlabel('optimal\n lambda', fontsize=10)
 plt.title(f'{l_save_name}', fontsize=7)
-# plt.savefig(f'{output_figs_dir}{l_save_name}.png', dpi=500)
+plt.savefig(f'{output_figs_dir}{l_save_name}.png', dpi=500)
 # plt.show()
 plt.close()
 
